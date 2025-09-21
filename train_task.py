182428971a8e9396160346e86b5a344c2b81377f
tarasstukalo <taras.stukalo0@gmail.com>
2025-09-21 20:12:54 +0300

    Created test cycle in train_task.py.

--- train_task.py
+++ train_task.py
@@ -1,3 +1,4 @@
+
 def max_speed(speed_list: list[float]) -> float:
     max_s = 0
     for speed in speed_list:
@@ -8,3 +9,14 @@ def max_speed(speed_list: list[float]) -> float:
 
 def avg_speed(speed_list: list[float]) -> float:
     return sum(speed_list) / len(speed_list)
+
+
+trip_stat = {
+    "I": [40.4, 28.9, 67.1, 21.3],
+    "II": [85.7, 78.3, 71.8, 51.3],
+    "III": [36.2, 65.2, 76.2, 90.4],
+    "VI": [56.4, 61.1, 76.2, 86.6,],
+}
+
+for key, value in trip_stat.items():
+    print({key: {"max_s": max_speed(value), "avg_s": avg_speed(value)}})
