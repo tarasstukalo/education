a9e3e6dfdb6f290451aa8daa21f21fcb022fa8bf
tarasstukalo <taras.stukalo0@gmail.com>
2025-09-26 21:48:12 +0300

    created new file train_task.py.

--- train_task.py
+++ train_task.py
@@ -1,4 +1,3 @@
-
 def max_speed(speed_list: list[float]) -> float:
     max_s = 0
     for speed in speed_list:
@@ -11,12 +10,19 @@ def avg_speed(speed_list: list[float]) -> float:
     return sum(speed_list) / len(speed_list)
 
 
-trip_stat = {
-    "I": [40.4, 28.9, 67.1, 21.3],
-    "II": [85.7, 78.3, 71.8, 51.3],
-    "III": [36.2, 65.2, 76.2, 90.4],
-    "VI": [56.4, 61.1, 76.2, 86.6,],
+def stat(trip_stat: dict) -> dict:
+    statistics = {}
+    for key, value in trip_stat.items():
+        statistics.update(({key: {"max_s": max_speed(value), "avg_s": avg_speed(value)}}))
+    return statistics
+
+
+my_stat = {
+        "I": [40.4, 28.9, 67.1, 21.3],
+        "II": [85.7, 78.3, 71.8, 51.3],
+        "III": [36.2, 65.2, 76.2, 90.4],
+        "VI": [56.4, 61.1, 76.2, 86.6, ],
 }
 
-for key, value in trip_stat.items():
-    print({key: {"max_s": max_speed(value), "avg_s": avg_speed(value)}})
+result = stat(my_stat)
+print(result)
