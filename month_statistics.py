be415dd9a5eade146b60d009d726d06b44b5fbd8
tarasstukalo <taras.stukalo0@gmail.com>
2025-09-28 16:16:12 +0300

    Created new function has_increased_scholarship. Finished function month_stat.

--- month_statistics.py
+++ month_statistics.py
@@ -1,16 +1,59 @@
-def month_stat(points:dict) -> dict:
+import pprint
+
+from train_task import max_speed
+
+
+def has_increased_scholarship(num: int | float) -> str:
+    if num >= 10:
+        return "Так, є підвищена степендія"
+    else:
+        return "Ні, немає"
+
+def month_stat(points: dict) -> tuple:
     total_stat = {}
+    total_points = []
     for key, value in points.items():
-        print(key, sum(value))
-        total_stat.update({key: sum(value)})
-    return points
+        total_points.extend(value)
+        summa = sum(value)
+        total_stat.update(
+            {
+                key:
+                    {
+                        "Загальна сума": summa,
+                        "Максимальний бал": max_speed(value),
+                        "Мінімальний бал": min(value),
+                        "Середній бал": summa / len(value)
+                    }
+            }
+        )
+    max_points = []
+    min_points = []
+    avg_points = []
+    total_avg_points = sum(total_points) / len(total_points)
+    for lesson_stat in total_stat.values():
+        max_points.append(lesson_stat["Максимальний бал"])
+        min_points.append(lesson_stat["Мінімальний бал"]),
+        avg_points.append(lesson_stat["Середній бал"])
+    lessons_stat = {
+        "Загальна статистика": {
+            "Загальна сума": sum(total_points),
+            "Максимальний бал": max(max_points),
+            "Мінімальний бал": min(min_points),
+            "Середній бал": total_avg_points,
+            "Підвищена степендія": has_increased_scholarship(total_avg_points)
+
+        }
+    }
+
+    return total_stat, lessons_stat
 
 
 if __name__ == "__main__":
     my_month_stat = {
         "Українська мова": [10, 11, 12, 10, 10, 12],
-        "Алгебра": [12, 12, 10, 11, 10, 10],
-        "Фізика": [10, 10, 12, 11, 11, 10]
+        "Алгебра": [12, 10, 10, 11, 10, 10],
+        "Фізика": [10, 10, 12, 11, 11, 10],
+        "Історія": [8, 5, 7, 10, 12, 3]
     }
-    res = month_stat(my_month_stat)
-    # print(res)
+    my_res = month_stat(my_month_stat)
+    pprint.pprint(my_res)
