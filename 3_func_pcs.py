47a8e339be5670e37ce8faa04f80d2334c156e4e
tarasstukalo <taras.stukalo0@gmail.com>
2025-10-01 21:42:41 +0300

    Updated file 3_func_pcs.py.

--- 3_func_pcs.py
+++ 3_func_pcs.py
@@ -1,12 +1,12 @@
-def get_hello() -> str:
-    return "Hello"
+def get_hello(word: str) -> str:
+    return word
 
-def get_wrld() ->str:
-    return "wrld"
+def get_wrld(word2: str) ->str:
+    return word2[::-1]
 
-def get_hello_world() -> tuple:
-    return get_hello(), get_wrld()
+def get_hello_world(word3) -> tuple:
+    return get_hello(word3), get_wrld(word3)
 
 if __name__ == "__main__":
-    first, second = get_hello_world()
+    first, second = get_hello_world("3vidramylyaki")
     print(first, second)
