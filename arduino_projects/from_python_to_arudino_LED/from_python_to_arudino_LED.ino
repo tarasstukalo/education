7df9f6e476fa1a45802c5c8ef8f3f707b339d55a
tarasstukalo <taras.stukalo0@gmail.com>
2026-01-22 23:10:27 +0500

    Created new file from_python_to_arudino_LED.ino.

---
+++ arduino_projects/from_python_to_arudino_LED/from_python_to_arudino_LED.ino
@@ -0,0 +1,47 @@
+int x = 0;
+
+void setup() {
+  Serial.begin(9600);
+  Serial.setTimeout(100);
+  pinMode(9, OUTPUT);
+  pinMode(10, OUTPUT);
+  pinMode(11, OUTPUT);
+
+}
+
+void loop() {
+  if (Serial.available()) {
+    x = Serial.readString().toInt();
+
+  digitalWrite(9, LOW);
+  digitalWrite(10, LOW);
+  digitalWrite(11, LOW);
+
+  if (x == 1) {
+    digitalWrite(9, HIGH); //blue 001
+  }
+  if (x == 2) {
+    digitalWrite(10, HIGH); //red 010
+  }
+  if (x == 3) {
+    digitalWrite(11, HIGH); //green 011
+  }
+  if (x == 4) {
+    digitalWrite(9, HIGH); //pink 100
+    digitalWrite(10, HIGH);
+  }
+  if (x == 5) {
+    digitalWrite(10, HIGH); //yellow 101
+    digitalWrite(11, HIGH);
+  }
+  if (x == 6) {
+    digitalWrite(11, HIGH); // light blue 110
+    digitalWrite(9, HIGH);
+  }
+    if (x == 6) {
+    digitalWrite(11, HIGH); // white 111
+    digitalWrite(10, HIGH);
+    digitalWrite(9, HIGH);
+  }
+}
+}
