dad6fd5af6f603cc02f95d2c697d9cce166507e6
tarasstukalo <taras.stukalo0@gmail.com>
2026-02-11 22:58:05 +0200

    Updated file relay_metronom_plus_button.ino.

---
+++ arduino_projects/relay_metronom_plus_button/relay_metronom_plus_button.ino
@@ -0,0 +1,104 @@
+int takt = 0;
+int lastTakt = -1;
+
+unsigned long lastBlink = 0;
+bool ledState = false;
+
+const int buttonPin = 11;
+bool lastButton = HIGH;
+
+void setup() {
+  pinMode(10, OUTPUT);
+
+  for (int i = 2; i <= 9; i++) {
+    pinMode(i, OUTPUT);
+  }
+
+  pinMode(buttonPin, INPUT_PULLUP);
+}
+
+void loop() {
+  bool button = digitalRead(buttonPin);
+
+  if (button == LOW && lastButton == HIGH) {
+    if (takt == 0) takt = 1;
+    else if (takt == 1) takt = 2;
+    else if (takt == 2) takt = 4;
+    else if (takt == 4) takt = 8;
+    else if (takt == 8) takt = 15;   // F
+    else takt = 0;
+
+    delay(200);
+  }
+
+  lastButton = button;
+
+  if (takt > 0 && millis() - lastBlink >= 1000 / takt) {
+    lastBlink = millis();
+    ledState = !ledState;
+    digitalWrite(10, ledState);
+  }
+
+  if (takt != lastTakt) {
+    lastTakt = takt;
+
+    for (int i = 2; i <= 9; i++) {
+      digitalWrite(i, HIGH);
+    }
+
+        if (takt == 0) {
+          digitalWrite(2, LOW);
+          digitalWrite(3, LOW);
+          digitalWrite(4, LOW);
+          digitalWrite(5, LOW);
+          digitalWrite(6, LOW);
+          digitalWrite(7, LOW);
+          digitalWrite(8, HIGH);
+      }
+      else if (takt == 1) {
+        digitalWrite(2, HIGH);
+        digitalWrite(3, LOW);
+        digitalWrite(4, LOW);
+        digitalWrite(5, HIGH);
+        digitalWrite(6, HIGH);
+        digitalWrite(7, HIGH);
+        digitalWrite(8, HIGH);
+      }
+      else if (takt == 2) {
+        digitalWrite(2, LOW);
+        digitalWrite(3, LOW);
+        digitalWrite(4, HIGH);
+        digitalWrite(5, LOW);
+        digitalWrite(6, LOW);
+        digitalWrite(7, HIGH);
+        digitalWrite(8, LOW);
+      }
+      else if (takt == 4) {
+        digitalWrite(2, HIGH);
+        digitalWrite(3, LOW);
+        digitalWrite(4, LOW);
+        digitalWrite(5, HIGH);
+        digitalWrite(6, HIGH);
+        digitalWrite(7, LOW);
+        digitalWrite(8, LOW);
+      }
+      else if (takt == 8) {
+        digitalWrite(2, LOW);
+        digitalWrite(3, LOW);
+        digitalWrite(4, LOW);
+        digitalWrite(5, LOW);
+        digitalWrite(6, LOW);
+        digitalWrite(7, LOW);
+        digitalWrite(8, LOW);
+      }
+      else if (takt == 15) {
+        digitalWrite(2, LOW);
+        digitalWrite(3, HIGH);
+        digitalWrite(4, HIGH);
+        digitalWrite(5, HIGH);
+        digitalWrite(6, LOW);
+        digitalWrite(7, LOW);
+        digitalWrite(8, LOW);
+      }
+  }
+}
